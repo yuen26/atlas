715c0097bc78ab6ec61c5c8c974b3d4ba7879c76
Andy <13061970+yuen26@users.noreply.github.com>
2024-08-15 00:51:03 +0700

    Observability

--- edge/discovery-server/eureka-server/src/main/java/org/atlas/discovery/server/eureka/EurekaServerApplication.java
+++ edge/discovery-server/eureka-server/src/main/java/org/atlas/discovery/server/eureka/EurekaServerApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+	"org.atlas.discovery.server.eureka",
+	"org.atlas.framework"
+})
 @EnableEurekaServer
 public class EurekaServerApplication {
 
--- framework/api/rest/rest-server/rest-server-core/src/main/java/org/atlas/framework/rest/server/core/logging/RequestLoggingFilter.java
+++ framework/api/rest/rest-server/rest-server-core/src/main/java/org/atlas/framework/rest/server/core/logging/RequestLoggingFilter.java
@@ -5,8 +5,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.atlas.framework.rest.server.core.idempotence.IdempotenceKeyGenerator;
-import org.slf4j.MDC;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -14,21 +12,11 @@ import java.io.IOException;
 @Slf4j
 public class RequestLoggingFilter extends OncePerRequestFilter {
 
-    private static final String IDEMPOTENCE_KEY = "x-request-id";
     private static final int MAX_PAYLOAD_LENGTH = 1024 * 1024; // 1 MB in bytes
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        String idempotenceKey = IdempotenceKeyGenerator.generate();
-
-        // Support the next requests to be able to retrieve the idempotence key
-        request.setAttribute(IDEMPOTENCE_KEY, idempotenceKey);
-
-        // Log idempotence key. Need to put it into log pattern. See logback-spring.xml file.
-        // Note: MDC does not work with asynchronous scenarios.
-        MDC.put(IDEMPOTENCE_KEY, idempotenceKey);
         HttpServletRequestUtil.logRequest(log, request, MAX_PAYLOAD_LENGTH);
-
         filterChain.doFilter(request, response);
     }
 }
