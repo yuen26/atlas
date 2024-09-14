4821cca5380d33954e804a05cfa3346d785fa3b9
Andy <13061970+yuen26@users.noreply.github.com>
2024-09-14 20:36:26 +0700

    local script

---
+++ edge/discovery-server/eureka-server/src/main/java/org/atlas/edge/discovery/eureka/EurekaServerApplication.java
@@ -0,0 +1,18 @@
+package org.atlas.edge.discovery.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication(scanBasePackages = {
+	"org.atlas.edge.discovery.eureka",
+	"org.atlas.framework"
+})
+@EnableEurekaServer
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
